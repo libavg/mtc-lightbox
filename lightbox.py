225090b5a044890a312dca7d39e007cfa03d2944
uzadow <uzadow@libavg.de>
2008-10-24 20:48:20 +0000

    added tracker bitmap overlay for debugging
    svn path=/trunk/avg_media/mtc/lightbox/; revision=3316

--- lightbox.py
+++ lightbox.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.5
 
 import os
+#import time
 from math import *
 from random import *
 from libavg import avg, anim, draggable
@@ -14,28 +15,41 @@ def populateLightbox():
     for fname in dirList:
         print "adding ",fname
         newImage=Player.createNode("image", {"href":path+"/"+fname})
-        Player.getRootNode().appendChild(newImage)
-        newImage.x=randint(10,300)
-        newImage.y=randint(5,200)
-        newImage.width /= 3
-        newImage.height /= 3
-        newImage.angle=radians(320)+uniform(0,radians(80))
+        Player.getElementByID("lightbox").appendChild(newImage)
+        root=Player.getRootNode()
+        newImage.x=randint(0,root.width/2)
+        newImage.y=randint(0,root.height/2)
+        newImage.width /= 4
+        newImage.height /= 4
+        newImage.angle=radians(330)+uniform(0,radians(60))
         imgDragger=draggable.Draggable(newImage)
         imgDragger.enable()
         imageList.append(imgDragger)
         
 def onFrame():
-    pass
+        Bitmap = Tracker.getImage(avg.IMG_FINGERS)
+        Node = Player.getElementByID("TrackerBitmap")
+        Node.setBitmap(Bitmap)
+        Node.width=1280
+        Node.height=720
+        Node.angle=pi
 
+def onClick():
+        pass
 
 Player = avg.Player()
 ##- Player.loadFile('lightbox.avg')
-Player.loadString('<avg width="1280" height="720"/>')
+Player.loadString('''
+<avg width="1280" height="720">
+<div id="lightbox">
+</div>
+<image id="TrackerBitmap" sensitive="False"/>
+</avg>''')
 Player.setResolution(1,0,0,0)
 Player.setVBlankFramerate(1)
 Player.setOnFrameHandler(onFrame)
-##- Player.addTracker()
+Tracker=Player.addTracker("trackerrc")
+Tracker.setDebugImages(True,True)
 anim.init(Player)
 populateLightbox()
 Player.play()
-
